491d879f5e57a86549c6a15ce44d899ec395019b
Arnab Kumar Shil <ruddra90@gmail.com>
2021-02-24 19:17:37 +0600

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const app = express()
+const router = express.Router();
+
+const {API_KEY, DOMAIN, MAILING_LIST } = process.env
+
+const mailgun = require('mailgun-js')({ apiKey: API_KEY, domain: DOMAIN });
+
+
+const list = mailgun.lists(`${MAILING_LIST}@${DOMAIN}`);
+
+const template = {
+  subscribed: true,
+}
+
+router.post('/', (req, res) => {
+  console.log(req)
+  let user = {
+    ...template,
+    ...req
+  }
+  list.members().create(user, function (error, data) {
+    res.json({
+      "success": true
+    })
+  });
+})
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
